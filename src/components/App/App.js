2962cfe9b8fb03f17ef19ce0ac325c89614450ec
Rachel Hendricks <rachelhen92@gmail.com>
2021-11-13 10:24:27 -0700

    Add useEffect hook and return Movies

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,29 @@
-import logo from '../images/logo.svg';
+import logo from '../../images/logo.svg';
+import './App.css';
+import React, { useState, useEffect } from 'react';
+import getRecentMovies from '../../api-calls';
+import Movies from '../Movies/Movies';
 
-const App = () => (
-	<img src={logo} alt="Timescale" />
-)
+const App = () => {
+	const [movies, setMovies] = useState([]);
+	const [error, setError] = useState('');
+
+	useEffect(() => {
+		getRecentMovies()
+		 	.then(data => {
+				console.log(process.env.REACT_APP_API_DOMAIN)
+				setMovies(data.results)
+			})
+			.catch(error => setError('Unable to show most recent movies'))
+	}, [])
+
+	return (
+		<main>
+			<img src={logo} alt="Timescale" />
+			<Movies movies={movies} />
+		</main>
+	)
+}
+
 
 export default App;
