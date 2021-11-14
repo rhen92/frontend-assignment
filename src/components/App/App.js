51f3d6027cb958b29bdbd83143a77811cfda0df2
Rachel Hendricks <rachelhen92@gmail.com>
2021-11-13 21:18:43 -0700

    Add functions to handleModal and showModal

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,10 +3,13 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 import getRecentMovies from '../../api-calls';
 import Movies from '../Movies/Movies';
+import Modal from '../Modal/Modal';
 
 const App = () => {
 	const [movies, setMovies] = useState([]);
 	const [error, setError] = useState('');
+	const [showModal, setShowModal] = useState(false);
+	const [modalSpotlight, setModalSpotlight] = useState([]);
 
 	useEffect(() => {
 		getRecentMovies()
@@ -15,6 +18,22 @@ const App = () => {
 			})
 			.catch(error => setError('Unable to show most recent movies'))
 	}, [])
+
+	const handleModal = (event) => {
+		setShowModal(!showModal);
+			const movieTitle = event.target.alt;
+			findModalSpotlight(movieTitle);
+	}
+
+	const findModalSpotlight = (title) => {
+		const spotlightMovie = movies.find(movie => movie.title === title);
+		setModalSpotlight(spotlightMovie);
+		if (spotlightMovie) {
+			document.body.className = 'modal-body';
+		} else {
+			document.body.className = 'none';
+		}
+	}
 
 	return (
 		<main>
@@ -30,7 +49,8 @@ const App = () => {
 				</div>
 			</div>
 			<hr />
-			<Movies movies={movies} />
+			<Movies movies={movies} handleModal={handleModal} />
+			{showModal && <Modal modalSpotlight={modalSpotlight} handleModal={handleModal} />}
 		</main>
 	)
 }
