f80488374eed88028947467d399e26531fa8c490
Rachel Hendricks <rachelhen92@gmail.com>
2021-11-13 21:19:13 -0700

    Add prop of handleModal to component

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Movies.css';
 import IndividualMovie from '../IndividualMovie/IndividualMovie';
 
-const Movies = ({ movies }) => {
+const Movies = ({ movies, handleModal }) => {
   const allMovies = movies.map((movie, index) => {
     return (
       <IndividualMovie
@@ -10,6 +10,7 @@ const Movies = ({ movies }) => {
       image={process.env.REACT_APP_API_BASE_IMAGE_URL + movie.poster_path}
       title={movie.title}
       score={movie.vote_average}
+      handleModal={handleModal}
       />
     )
   })
