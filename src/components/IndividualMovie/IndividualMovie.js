99b6504615f949cc7155e66400d5418814d714aa
Rachel Hendricks <rachelhen92@gmail.com>
2021-11-13 21:17:39 -0700

    Add className to section

--- src/components/IndividualMovie/IndividualMovie.js
+++ src/components/IndividualMovie/IndividualMovie.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './IndividualMovie.css';
 
-const IndividualMovie = ({ image, title, score}) => {
+const IndividualMovie = ({ image, title, score, handleModal }) => {
   return (
-    <section className='movie-card'>
+    <section className='movie-card' onClick={handleModal}>
       <p className='score'>{score}</p>
       <img className='poster-image' src={image} alt={title}/>
       <h1 className='title'>{title}</h1>
